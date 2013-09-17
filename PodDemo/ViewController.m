91602554e3d890c0d59d0ba876f7b94a7957e617
wufulin <wufulinit@gmail.com>
2013-09-17 15:28:36 +0800

    试验XLRemoteImageView功能

--- PodDemo/ViewController.m
+++ PodDemo/ViewController.m
@@ -7,11 +7,15 @@
 //
 
 #import "ViewController.h"
+#import "WEPopoverController.h"
+#import "WEPopoverContentViewController.h"
+#import "UIImageView+XLProgressIndicator.h"
 
 #define IMAGEURL @"http://etonvision.com/wp-content/uploads/2013/08/DSC_0693.jpg"
 
-@interface ViewController () {
+@interface ViewController ()<WEPopoverControllerDelegate> {
 	UIImageView *imageView;
+	WEPopoverController *popoverController;
 }
 @end
 
@@ -26,6 +30,11 @@
     return self;
 }
 
+- (id)initializeWithName:(NSString *)name
+{
+	return nil;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -34,15 +43,56 @@
 	[imageView setContentMode:UIViewContentModeScaleAspectFit];
 	[self.view addSubview:imageView];
 	
+	// progress color, yellow color in the example image.
+	[[XLCircleProgressIndicator appearance] setStrokeProgressColor:[UIColor yellowColor]];
+	
+	// remaining color, gray color in the example image
+	[[XLCircleProgressIndicator appearance] setStrokeRemainingColor:[UIColor grayColor]];
+	
+	//In order to set up the circle stroke's width you can choose between these 2 ways.
+	[[XLCircleProgressIndicator appearance] setStrokeWidth:3.0f];
+
+	[self.view setBackgroundColor:[UIColor blackColor]];
+	UIImageView *newImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 320, CGRectGetHeight(self.view.frame))];
+	[newImageView setContentMode:UIViewContentModeScaleAspectFit];
+	[newImageView setImageWithProgressIndicatorAndURL:[NSURL URLWithString:@"http://h.hiphotos.baidu.com/album/w%3D2048/sign=1b4d99a0cdbf6c81f7372be88806b335/9345d688d43f8794230d35e6d31b0ef41ad53a03.jpg"]];
+	[self.view addSubview:newImageView];
+	
+	
 	NSInvocationOperation *operation = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(downloadImage:) object:IMAGEURL];
 	NSOperationQueue *operationQueue = [[NSOperationQueue alloc] init];
 	[operationQueue addOperation:operation];
+	
+	UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+	[button setFrame:CGRectMake(0.0, 110.0, 100, 44)];
+	[button setTintColor:[UIColor blueColor]];
+	[button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+	[button setTitle:@"abc" forState:UIControlStateNormal];
+	[self.view addSubview:button];
+	
+	[button addTarget:self action:@selector(didClick:) forControlEvents:UIControlEventTouchUpInside];
+	
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)didClick:(id)sender {
+	UIButton *button = (UIButton *)sender;
+	if (!popoverController) {
+		WEPopoverContentViewController *tableView = [[WEPopoverContentViewController alloc] initWithStyle:UITableViewStylePlain];
+		popoverController = [[WEPopoverController alloc] initWithContentViewController:tableView];
+		popoverController.delegate = self;
+	}
+	
+	[popoverController presentPopoverFromRect:button.frame
+									   inView:self.view
+					 permittedArrowDirections:UIPopoverArrowDirectionAny
+									 animated:YES];
+	
 }
 
 - (void)downloadImage:(NSString *)url {
@@ -60,5 +110,15 @@
 
 + (ViewController *)create {
 	return nil;
+}
+
+#pragma mark -
+#pragma mark WEPopoverController Delegate
+- (void)popoverControllerDidDismissPopover:(WEPopoverController *)popoverController {
+	
+}
+
+- (BOOL)popoverControllerShouldDismissPopover:(WEPopoverController *)popoverController{
+	return YES;
 }
 @end
